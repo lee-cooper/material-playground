d724962f27372bab27254614e1bc832e9e18842b
Lee Cooper <cooperlee372@gmail.com>
2021-12-18 17:21:39 -0500

    add the home component. Add routing module and hook up basic routes.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AngularMaterialModule } from './angular-material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AngularMaterialModule],
+  declarations: [AppComponent, HeaderComponent, ProfileComponent, HomeComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AngularMaterialModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
