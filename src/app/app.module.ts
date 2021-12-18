c7b52b9b6ac75ea9a9c57d735996406bcac16ae0
Lee Cooper <cooperlee372@gmail.com>
2021-12-18 17:25:45 -0500

    re-org folder structure and add barrel file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,22 @@ import { AngularMaterialModule } from './angular-material.module';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ProfileComponent } from './profile/profile.component';
-import { HomeComponent } from './home/home.component';
+import { HeaderComponent, HomeComponent, ProfileComponent } from './components';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ProfileComponent, HomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AngularMaterialModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/components/index.ts
@@ -0,0 +1,3 @@
+export { ProfileComponent } from './profile/profile.component';
+export { HomeComponent } from './home/home.component';
+export { HeaderComponent } from './header/header.component';
