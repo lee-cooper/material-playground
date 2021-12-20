8258bbb0bc936cdc14f1bdb8e3601513d4fd559c
Lee Cooper <cooperlee372@gmail.com>
2021-12-19 20:22:20 -0500

    refactor to use mat toolbar and menu

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -3,9 +3,35 @@ import { NgModule } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [MatInputModule, MatFormFieldModule, MatSelectModule],
-  exports: [MatInputModule, MatFormFieldModule, MatSelectModule],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
+  exports: [
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+  ],
 })
 export class AngularMaterialModule {}
